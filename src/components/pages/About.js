8fade942f52eebc22e1ff6c85102c412fbf06654
Koshana <IT18037920@my.sliit.lk>
2020-09-15 15:29:52 +0530

    First Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About this App</h1>
+            <p>App to serach Github users</p>
+            <p>Koshana Pravinath 5/22/2020 Udemy React Front to Back Course Project 01</p>
+        </Fragment>
+    )
+}
+
+export default About
